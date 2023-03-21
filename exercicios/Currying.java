5791935e59edf38ed342d1c5d44a7e88e2ceb715
Samuel Ferraz <ssf@discente.ifpe.edu.br>
2023-03-21 18:09:04 -0300

    Update Currying.java

--- exercicios/Currying.java
+++ exercicios/Currying.java
@@ -6,23 +6,17 @@ import java.util.stream.Collectors;
 
 public class Currying {
 
-	/*
-	 * Currying é uma técnica em que uma função com múltiplos 
-	 * argumentos é transformada em uma sequência de funções
-	 * com um único argumento.
-	 */
-
 	public static void main(String[] args) {
 		List<Integer> numeros = List.of(1, 2, 3, 4, 5);
 
-		Function<Integer, Function<Integer, Integer>> soma = x -> y -> x + y;
+		Function<Integer, Function<Integer, Integer>> 
+		soma = x -> y -> x + y;
 
 		var resultado = numeros.stream()
 				.map(soma.apply(2))
 				.collect(Collectors.toList());
 
-		System.out.println(resultado); // Imprime [1, 4, 9, 16, 25]
-
+		System.out.println(resultado); // [3, 4, 5, 6, 7]
 	}
 
 }
