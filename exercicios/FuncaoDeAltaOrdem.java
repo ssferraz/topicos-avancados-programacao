dfad8ee79589039f5aa9c0c7adc0b6ce21b91646
Samuel Ferraz <ssf@discente.ifpe.edu.br>
2023-03-21 18:09:23 -0300

    Update FuncaoDeAltaOrdem.java

--- exercicios/FuncaoDeAltaOrdem.java
+++ exercicios/FuncaoDeAltaOrdem.java
@@ -5,16 +5,12 @@ import java.util.function.IntBinaryOperator;
 public class FuncaoDeAltaOrdem {
 
 	public static void main(String[] args) {
-		/*
-		 * Uma função de alta ordem é uma função que recebe outra função 
-		 * como argumento ou retorna uma função como resultado. 
-		 */
 		
 		IntBinaryOperator soma = (x, y) -> x + y;
 		
 		var resultado = soma.applyAsInt(5, 3);
 		
-		System.out.println(resultado);
+		System.out.println(resultado); // 8
 	}
 
 }
