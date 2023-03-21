a23ff4410eee541d28290223e6fe2141eb9e2a51
Samuel Ferraz <ssf@discente.ifpe.edu.br>
2023-03-21 18:09:46 -0300

    Update FuncoesPuras.java

--- exercicios/FuncoesPuras.java
+++ exercicios/FuncoesPuras.java
@@ -1,16 +1,13 @@
 package exercicios;
 
 public class FuncoesPuras {
-
-	/*
-	 * Funções puras são funções que não têm efeitos colaterais e 
-	 * sempre produzem o mesmo resultado para os mesmos argumentos. 
-	 * Em outras palavras, uma função pura não altera nenhum estado externo 
-	 * e não depende de nenhum estado externo.
-	 */
 	
 	public static int soma(int a, int b) {
         return a + b;
     }
+	
+	public static void main(String[] args) {
+		System.out.println(soma(1, 3)); // 4 
+	}
 	
 }
